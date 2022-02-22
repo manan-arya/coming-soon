465b01d65c3a66b2494a9b7c3b6749adb4ce9d1e
Manan Arya <manan.arya@ihsmarkit.com>
2022-02-22 20:29:29 +0530

    before firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactTypingEffect from 'react-typing-effect';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <ReactTypingEffect
+        text={["Coming Soon.", "We don't need to wait for our website to help you."]}
+        staticText = {<h1>StructureKart</h1>}
+        cursorRenderer={cursor => <h1>{cursor}</h1>}
+        displayTextRenderer={(text) => <h1>{text}</h1>}
+        speed = {200}       
+      />
+      <br/>
+      <form>
+        <label for="fname">First name:</label>
+        <input type="text" id="fname" name="fname">
+        </input><br/>
+        <label for="lname">Last name:</label>
+        <input type="text" id="lname" name="lname"></input>
+        <br/>
+        <input type="button" id="submit" name = "submit"></input>
+      </form>
     </div>
   );
 }
